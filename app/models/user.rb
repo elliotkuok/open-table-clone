79bd7c6fda8086acadb1501b5dd233fb0bdbf82b
Elliot Kuok <elliotckuok@gmail.com>
2023-08-13 20:22:22 -0700

    Add user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :email, :session_token, :first_name, :last_name, :phone_number, presence: true
+  validates :email, :session_token, :phone_number, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, length: { in: 3..255 }
+  validates :password, length: { in: 6..255 }, allow_nil: true
+
+  has_many :reservations
+
+  has_many :reviews,
+    through: :reservations
 end
