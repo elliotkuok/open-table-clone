4bc4ab84b699d10613a7cbe6edb956eae60b603f
Elliot Kuok <elliotckuok@gmail.com>
2023-08-14 12:15:54 -0700

    Seeding user instances

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,34 +6,36 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# db/seeds.rb
+ApplicationRecord.transaction do 
+    puts "Destroying tables..."
+    # Unnecessary if using `rails db:seed:replant`
+    User.destroy_all
 
-# ApplicationRecord.transaction do 
-#     puts "Destroying tables..."
-#     # Unnecessary if using `rails db:seed:replant`
-#     User.destroy_all
+    puts "Resetting primary keys..."
+    # For easy testing, so that after seeding, the first `User` has `id` of 1
+    ApplicationRecord.connection.reset_pk_sequence!('users')
 
-#     puts "Resetting primary keys..."
-#     # For easy testing, so that after seeding, the first `User` has `id` of 1
-#     ApplicationRecord.connection.reset_pk_sequence!('users')
+    puts "Creating users..."
+    # Create a demo user
+    User.create!(
+        email: 'demo@user.io', 
+        password: 'password',
+        first_name: 'Demo',
+        last_name: 'User',
+        phone_number: '5103626446'
+    )
 
-#     puts "Creating users..."
-#     # Create a demo user
-#     User.create!(
-#         username: 'Demo-lition', 
-#         email: 'demo@user.io', 
-#         password: 'password'
-#     )
+    # Create additional users
+    10.times do 
+        User.create!({
+        email: Faker::Internet.unique.email,
+        password: Faker::Internet.password(min_length: 6),
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        phone_number: Faker::PhoneNumber.phone_number
+        }) 
+    end
 
-#     # Create additional users
-#     10.times do 
-#         User.create!({
-#         username: Faker::Internet.unique.username(specifier: 3),
-#         email: Faker::Internet.unique.email,
-#         password: 'password'
-#         }) 
-#     end
-
-#     puts "Done!"
-# end
+    puts "Done!"
+end
   
