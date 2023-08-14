33e55bdb5403fad65ab94c019838742409d7138c
Elliot Kuok <elliotckuok@gmail.com>
2023-08-14 15:23:36 -0700

    Add working code to session and users controller methods

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,10 +1,29 @@
 class Api::SessionsController < ApplicationController
   def show
+    if current_user
+      @user = current_user
+      # render 'api/users/show'
+      render json: { user: current_user }
+    else
+      render json: { user: nil }
+    end
   end
 
   def create
+    @user = User.find_by_credentials(params[:email], params[:password])
+
+    if @user
+      login!(@user)
+      # render 'api/users/show'
+      render json: { user: @user }
+    else
+      render json: { errors: ['The provided credentials were invalid.'] }, 
+        status: :unauthorized
+    end
   end
 
   def destroy
+    logout!
+    render json: { message: 'success' }
   end
 end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,7 +1,23 @@
 class Api::UsersController < ApplicationController
+  wrap_parameters include: User.attribute_names + ['password']
+
   def show
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      login!(@user)
+      # render :show
+      render json: @user
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :first_name, :last_name, :phone_number)
   end
 end
