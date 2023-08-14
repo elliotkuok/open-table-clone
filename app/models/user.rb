e5e91112371d280d068aa662c52e4684d80f909e
Elliot Kuok <elliotckuok@gmail.com>
2023-08-14 12:00:24 -0700

    Complete User model with working authentication methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,31 @@ class User < ApplicationRecord
 
   has_many :reviews,
     through: :reservations
+
+  before_validation :ensure_session_token
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    user&.authenticate(password)
+  end
+
+  def reset_session_token!
+    self.session_token = generate_unique_session_token
+    self.save!
+    self.session_token
+  end
+
+  private
+  
+  def generate_unique_session_token
+   token = SecureRandom::urlsafe_base64
+   while User.exists?(session_token: token)
+    token = SecureRandom::urlsafe_base64
+   end
+   token
+  end
+  
+  def ensure_session_token
+    self.session_token ||= generate_unique_session_token
+  end
 end
