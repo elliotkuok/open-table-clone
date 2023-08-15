a085d66f1b55fb98a342037619c42b01efc9e128
Elliot Kuok <elliotckuok@gmail.com>
2023-08-15 00:34:56 -0700

    Add CSRF token; change frontend/src/index to react 17

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,22 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import configureStore from './store';
+import csrfFetch, { restoreCSRF } from './store/csrf';
 
-ReactDOM.createRoot(document.getElementById('root'))
-  .render(
+// ReactDOM.createRoot(document.getElementById('root'))
+//   .render(
+//     <React.StrictMode>
+//       <App />
+//     </React.StrictMode>
+//   )
+const renderApplication = () => {
+  ReactDOM.render(
     <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  )
+      <Root />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production') {
   window.store = store;
+  window.csrfFetch = csrfFetch;
 }
 
 function Root() {
@@ -27,4 +37,10 @@ function Root() {
       </BrowserRouter>
     </Provider>
   );
+}
+
+if (sessionStorage.getItem("X-CSRF-Token") === null) {
+  restoreCSRF().then(renderApplication);
+} else {
+  renderApplication();
 }
