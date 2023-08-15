cb5065ae94e015b3f8f3fba8da618e550df6fc1a
Elliot Kuok <elliotckuok@gmail.com>
2023-08-14 23:19:35 -0700

    Set up Redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import configureStore from './store';
 
 ReactDOM.createRoot(document.getElementById('root'))
   .render(
@@ -9,3 +12,19 @@ ReactDOM.createRoot(document.getElementById('root'))
     <App />
   </React.StrictMode>
   )
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
+
+function Root() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+}
