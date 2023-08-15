b9b1028914aab59b4c1561ee0288750084f3193e
Elliot Kuok <elliotckuok@gmail.com>
2023-08-14 21:57:21 -0700

    Add CSRF protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::API
+    # If your ApplicationController inherited from ActionController::Base instead of ActionController::API, then this module would already be included with the protection turned on by default.
+    include ActionController::RequestForgeryProtection
+    protect_from_forgery with: :exception
     helper_method :current_user, :logged_in?
-    before_action :snake_case_params
+    before_action :snake_case_params, :attach_authenticity_token
 
     def current_user
         @current_user ||= User.find_by(session_token: session[:session_token])
@@ -24,9 +27,27 @@ class ApplicationController < ActionController::API
         @current_user = nil
     end
 
+    def test
+        if params.has_key?(:login)
+          login!(User.first)
+        elsif params.has_key?(:logout)
+          logout!
+        end
+      
+        if current_user
+          render json: { user: current_user.slice('id', 'email', 'session_token') }
+        else
+          render json: ['No current user']
+        end
+    end
+
     private
     
     def snake_case_params
-      params.deep_transform_keys!(&:underscore)
+        params.deep_transform_keys!(&:underscore)
+    end
+
+    def attach_authenticity_token
+        headers['X-CSRF-Token'] = masked_authenticity_token(session)
     end
 end
