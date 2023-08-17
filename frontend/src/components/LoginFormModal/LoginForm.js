fb81ead0a619ce25f3490a79515548c2a909853c
Elliot Kuok <elliotckuok@gmail.com>
2023-08-17 09:44:28 -0700

    Add x to close loginform modal

--- frontend/src/components/LoginFormModal/LoginForm.js
+++ frontend/src/components/LoginFormModal/LoginForm.js
@@ -164,6 +164,10 @@ function LoginForm({onClose}) {
     setContinueButtonDisabled(false);
     setEmail(submittedEmail);
   };
+  
+  const handleClose = () => {
+    onClose();
+  };
 
   const isEmailValid = (email) => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -175,10 +179,27 @@ function LoginForm({onClose}) {
     {sessionUser && <Redirect to="/" />}
     <div className="login-form">
       {showPasswordInput && (
-      <i className="back-icon" onClick={handleBack}>
-        &lt; {/* Use HTML entity for "<" */}
-      </i>
+        <i className="back-icon" onClick={handleBack}>
+          &lt; {/* Use HTML entity for "<" */}
+        </i> 
     )}
+        <div className="x-icon" onClick={handleClose}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <line x1="18" y1="6" x2="6" y2="18" />
+            <line x1="6" y1="6" x2="18" y2="18" />
+          </svg>
+        </div>
+
 
     <form onSubmit={handleSubmit}>
       <ul>
