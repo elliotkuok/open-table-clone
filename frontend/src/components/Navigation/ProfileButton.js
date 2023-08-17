10e39781ac700c41dae51a6b9d20b6dce03fdad9
Elliot Kuok <elliotckuok@gmail.com>
2023-08-17 01:03:52 -0700

    Add profile button with styling

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -4,8 +4,35 @@ import * as sessionActions from '../../store/session';
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 function ProfileButton({ user }) {
-  const dispatch = useDispatch();
-  
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+
+  useEffect(() => {
+    // Fetch user data using the user's id or email
+    fetchUserData();
+  }, []);
+
+  const fetchUserData = async () => {
+    try {
+      const response = await fetch(`/api/users/${user.id}`); // Replace with your API endpoint
+      const userData = await response.json();
+      setFirstName(userData.user.firstName);
+      setLastName(userData.user.lastName);
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    }
+  };
+
+  const firstInitial = firstName ? firstName.charAt(0) : '';
+  const lastInitial = lastName ? lastName.charAt(0) : '';
+
+  return (
+    <button id="profile-button">
+      <span style={{ marginTop: '3px' }}>
+        {firstInitial}{lastInitial}
+      </span>
+    </button>
+  );
 }
 
 export default ProfileButton;
