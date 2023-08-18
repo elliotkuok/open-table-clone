3263d1282d11294f0620dc720279e7732ef02fe0
Elliot Kuok <elliotckuok@gmail.com>
2023-08-18 09:56:09 -0700

    Remove logger

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -8,19 +8,8 @@ const rootReducer = combineReducers({
     errors: errorsReducer
 });
 
-let enhancer;
-
-if (process.env.NODE_ENV === 'production') {
-    enhancer = applyMiddleware(thunk);
-} else {
-    const logger = require('redux-logger').default;
-    const composeEnhancers =
-        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-    enhancer = composeEnhancers(applyMiddleware(thunk, logger));
-}
-
 const configureStore = (preloadedState) => {
-    return createStore(rootReducer, preloadedState, enhancer);
+    return createStore(rootReducer, preloadedState, applyMiddleware(thunk));
 };
 
 export default configureStore;
