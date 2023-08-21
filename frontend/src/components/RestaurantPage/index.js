a019c31d96196d51f241f6c31a2affef22163ce5
Elliot Kuok <elliotckuok@gmail.com>
2023-08-20 18:02:47 -0700

    Add elements to restaurant page info section with minor styling

--- frontend/src/components/RestaurantPage/index.js
+++ frontend/src/components/RestaurantPage/index.js
@@ -10,6 +10,39 @@ const RestaurantPage = () => {
                     <img className="banner-image" src={restaurantImage} alt="restaurant" />
                 </div> */}
                 <div className="restaurant-banner" style={{ backgroundImage: `url(${restaurantImage})` }}></div>
+                <div className="restaurant-container">
+                    <div className='restaurant-info'>
+                        <div className='restaurant-tabs'>
+                            <a href="#overview">
+                                Overview
+                            </a>
+                            <a href="#reviews">
+                                Reviews
+                            </a>
+                        </div>
+                        <div className='overview'>
+                            <h1>Restaurant Name</h1>
+                            <div className='overview-info'>
+                                <div>Rating Info</div>
+                                <div>Review no.</div>
+                                <div>Price range</div>
+                                <div>Cuisine</div>
+                            </div>
+                            <div className='restaurant-description'>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                    Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                                    laboris nisi ut aliquip ex ea commodo consequat. 
+                                    Duis aute irure dolor in reprehenderit in voluptate velit 
+                                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
+                                    cupidatat non proident, sunt in culpa qui officia 
+                                    deserunt mollit anim id est laborum.</p>
+                            </div>
+                            <div className='reviews'></div>
+                        </div>
+                    </div>
+                    <div className='reservation-container'></div>
+                </div>
             </div>
         </div>
     )
