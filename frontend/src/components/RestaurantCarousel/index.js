c66578e913e4eb08d2db2f92403a43e771cc4bbb
Elliot Kuok <elliotckuok@gmail.com>
2023-08-21 15:30:07 -0700

    Add nav buttons to carousel

--- frontend/src/components/RestaurantCarousel/index.js
+++ frontend/src/components/RestaurantCarousel/index.js
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import { fetchRestaurants, selectAllRestaurants } from "../../store/restaurants";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import React from 'react';
 import RestaurantTile from '../RestaurantIndex/RestaurantTile';
 import './RestaurantCarousel.css';
@@ -8,10 +8,27 @@ import './RestaurantCarousel.css';
 const RestaurantCarousel = () => {
     const restaurants = useSelector(selectAllRestaurants)
     const dispatch = useDispatch()
+    const [scrollAmount, setScrollAmount] = useState(0);
     
     useEffect(() => {
       dispatch(fetchRestaurants())
     },[dispatch])
+
+    const scrollLeft = () => {
+        const carousel = document.querySelector(".restaurant-carousel");
+        const itemWidth = carousel.querySelector(".carousel-item").offsetWidth;
+        const scrollWidth = itemWidth * 5;
+        setScrollAmount(scrollAmount - scrollWidth);
+        carousel.scrollTo({ left: scrollAmount - scrollWidth, behavior: "smooth" });
+    };
+    
+    const scrollRight = () => {
+        const carousel = document.querySelector(".restaurant-carousel");
+        const itemWidth = carousel.querySelector(".carousel-item").offsetWidth;
+        const scrollWidth = itemWidth * 5;
+        setScrollAmount(scrollAmount + scrollWidth);
+        carousel.scrollTo({ left: scrollAmount + scrollWidth, behavior: "smooth" });
+    };
 
     const thaiRestaurants = Object.values(restaurants).filter(restaurant => restaurant.cuisine === "Thai");
 
@@ -21,11 +38,13 @@ const RestaurantCarousel = () => {
                 <h3>Thai Restaurants</h3>
             </div>
             <div className="restaurant-carousel">
+                <button className="carousel-button left-button" onClick={scrollLeft}>&lt;</button>
                 {thaiRestaurants.map(restaurant => (
                     <div key={restaurant.id} className="carousel-item restaurant-index">
                         <RestaurantTile restaurant={restaurant} />
                     </div>
                 ))}
+                <button className="carousel-button right-button" onClick={scrollRight}>&gt;</button>
             </div>
         </div>
     );
