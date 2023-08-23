92b6eb1d021901897fa3d11be5255026aab67f7a
Elliot Kuok <elliotckuok@gmail.com>
2023-08-22 21:26:25 -0700

    Add ReservationsController and actions

---
+++ app/controllers/api/reservations_controller.rb
@@ -0,0 +1,47 @@
+class Api::ReservationsController < ApplicationController
+  def create
+    @reservation = Reservation.new(reservation_params)
+    if @reservation.save
+      render :show
+    else
+      render json: { errors: @reservation.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @reservation = Reservation.find_by(id: params[:id])
+    render :show
+  end
+
+  def update
+    @reservation = Reservation.find_by(id: params[:id])
+    if @reservation.update(reservation_params)
+      render :show
+    else
+      render json: { errors: @reservation.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @reservation = Reservation.find_by(id: params[:id])
+    if @reservation
+      if @reservation.user_id == current_user.id && @reservation.destroy
+        render :show
+      else
+        render json: { error: "Unauthorized reservation deletion" }, status: :not_found
+      end
+    else
+      render json: { error: "Reservation not found" }, status: :not_found
+    end
+  end
+
+  def index
+    @reservations = Reservation.all
+    render :index
+  end
+
+  private
+  def reservation_params
+    params.require(:reservation).permit(:restaurant_id, :user_id, :review_id, :date, :time, :party_size, :occasion, :special_request)
+  end
+end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,5 +1,8 @@
 class Reservation < ApplicationRecord
+  validates :date, :time, :party_size, presence: true
+  validates :party_size, numericality: { greater_than: 0 }
+
   belongs_to :restaurant
   belongs_to :user
-  belongs_to :review
+  belongs_to :review, optional: true
 end
