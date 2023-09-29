07ccd28e938280ff817f9bb658b7e40d3c01f76d
Elliot Kuok <elliotckuok@gmail.com>
2023-09-29 13:03:10 -0400

    WIP update attribute calling syntax

--- frontend/src/components/ModifyReservationPage/index.js
+++ frontend/src/components/ModifyReservationPage/index.js
@@ -2,7 +2,7 @@ import { useDispatch, useSelector } from "react-redux";
 import './ModifyReservationPage.css';
 import "../SearchSplash/SearchSplash.css";
 import { fetchReservation, selectReservation } from "../../store/reservations";
-import { selectRestaurant } from "../../store/restaurants";
+import { fetchRestaurant, selectRestaurant } from "../../store/restaurants";
 import { useEffect, useRef, useState } from "react";
 import { useHistory, useParams } from "react-router-dom/cjs/react-router-dom";
 import datePicker from 'js-datepicker';
@@ -14,12 +14,19 @@ const ModifyReservationPage = () => {
     const {id} = useParams();
     const dispatch = useDispatch();
     const reservation = useSelector(selectReservation(id));
-    const restaurant = useSelector(selectRestaurant(id));
+    const restaurantId = reservation?.restaurantId;
+    let restaurant = dispatch(fetchRestaurant(restaurantId))
 
     let openingTime, closingTime;
+
     if (restaurant && restaurant.hours) {
     [openingTime, closingTime] = restaurant.hours.split(' - ');
     }
+
+    console.log("restaurant:", restaurant)
+    console.log("hours:", restaurant.hours)
+    console.log("opening:", openingTime)
+    console.log("closing:", closingTime)
     const [selectedDate, setChosenDate] = useState(new Date());
     const [suggestedTimes, setSuggestedTimes] = useState([]);
 
@@ -27,6 +34,7 @@ const ModifyReservationPage = () => {
 
     useEffect(() => {
         dispatch(fetchReservation(id));
+        dispatch(fetchRestaurant(restaurantId));
       }, [dispatch, id]);
 
       const datePickerRef = useRef(null);
@@ -58,12 +66,12 @@ const ModifyReservationPage = () => {
     }
 
     const convertToMinutes = (time) => {
-        const [hoursMinutes, period] = time.split(' ');
-        let [hours, minutes] = hoursMinutes.split(':');
-        hours = parseInt(hours);
-        if (period === 'PM' && hours < 12) hours += 12;
-        if (period === 'AM' && hours === 12) hours = 0;
-        return hours * 60 + parseInt(minutes);
+        // const [hoursMinutes, period] = time.split(' ');
+        // let [hours, minutes] = hoursMinutes.split(':');
+        // hours = parseInt(hours);
+        // if (period === 'PM' && hours < 12) hours += 12;
+        // if (period === 'AM' && hours === 12) hours = 0;
+        // return hours * 60 + parseInt(minutes);
     };
 
     const convertTo12HourFormat = (minutes) => {
--- frontend/src/store/reservations.js
+++ frontend/src/store/reservations.js
@@ -76,6 +76,7 @@ export const fetchReservation = id => async (dispatch) => {
     const res = await csrfFetch(`/api/reservations/${id}`);
     if (res.ok) {
         const reservation = await res.json();
+        console.log("Fetched reservation:", reservation); // Debugging line
         dispatch(receiveReservation(reservation));
     }
 }
@@ -189,7 +190,13 @@ const reservationsReducer = (state = initialState, action) => {
             }
                 return nextState;
         case RECEIVE_RESERVATION:
+            // nextState[action.payload.reservation.id] = action.payload.reservation;
+            // return nextState;
+            case RECEIVE_RESERVATION:
+            console.log("Reducer state before:", nextState); // Debugging line
+            console.log("Payload received:", action.payload.reservation); // Debugging line
             nextState[action.payload.reservation.id] = action.payload.reservation;
+            console.log("Reducer state after:", nextState); // Debugging line
             return nextState;
         case RECEIVE_RESERVATIONS:
             return Object.assign(nextState, action.payload);
