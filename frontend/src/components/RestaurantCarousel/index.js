3da5d3be5ea19cfc24fd9bcf2d065ef02872e269
Elliot Kuok <elliotckuok@gmail.com>
2024-01-29 11:50:25 -0800

    Fix carousel buttons bug

--- frontend/src/components/RestaurantCarousel/index.js
+++ frontend/src/components/RestaurantCarousel/index.js
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import { fetchRestaurants, selectAllRestaurants } from "../../store/restaurants";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import React from 'react';
 import RestaurantTile from '../RestaurantIndex/RestaurantTile';
 import './RestaurantCarousel.css';
@@ -10,44 +10,49 @@ const CuisineCarousel = ({ cuisine }) => {
     const restaurants = useSelector(selectAllRestaurants);
     const dispatch = useDispatch();
     const [scrollAmount, setScrollAmount] = useState(0);
+    const carouselRef = useRef(null);
 
     useEffect(() => {
       dispatch(fetchRestaurants());
     }, [dispatch]);
 
+    const cuisineRestaurants = Object.values(restaurants).filter(restaurant => restaurant.cuisine === cuisine);
+
     const scrollLeft = () => {
-        const carousel = document.querySelector(`.${cuisine}-carousel`);
-        const itemWidth = carousel.querySelector(".carousel-item").offsetWidth;
-        const scrollWidth = itemWidth * 5;
-        setScrollAmount(scrollAmount - scrollWidth);
-        carousel.scrollTo({ left: scrollAmount - scrollWidth, behavior: "smooth" });
+        if (carouselRef.current) {
+            const itemWidth = carouselRef.current.querySelector(".carousel-item").offsetWidth;
+            const scrollWidth = itemWidth * 5;
+            const newScrollAmount = scrollAmount - scrollWidth;
+            setScrollAmount(newScrollAmount);
+            carouselRef.current.scrollTo({ left: newScrollAmount, behavior: "smooth" });
+        }
     };
 
     const scrollRight = () => {
-        const carousel = document.querySelector(`.${cuisine}-carousel`);
-        const itemWidth = carousel.querySelector(".carousel-item").offsetWidth;
-        const scrollWidth = itemWidth * 5;
-        setScrollAmount(scrollAmount + scrollWidth);
-        carousel.scrollTo({ left: scrollAmount + scrollWidth, behavior: "smooth" });
+        if (carouselRef.current) {
+            const itemWidth = carouselRef.current.querySelector(".carousel-item").offsetWidth;
+            const scrollWidth = itemWidth * 5;
+            const newScrollAmount = scrollAmount + scrollWidth;
+            setScrollAmount(newScrollAmount);
+            carouselRef.current.scrollTo({ left: newScrollAmount, behavior: "smooth" });
+        }
     };
-
-    const cuisineRestaurants = Object.values(restaurants).filter(restaurant => restaurant.cuisine === cuisine);
 
     return (
         <div className={`carousel-container ${cuisine}-carousel`}>
             <div className="carousel-header">
                 <h3>{cuisine} Restaurants</h3>
             </div>
-            <div className="restaurant-carousel-container"> {/* Added this wrapper */}
-                <div className="restaurant-carousel">
+            <div className="restaurant-carousel-container">
+                <div className="restaurant-carousel" ref={carouselRef}>
                     {cuisineRestaurants.map(restaurant => (
                         <Link to={`/restaurants/${restaurant.id}`} key={restaurant.id} className="carousel-item restaurant-index">
                             <RestaurantTile restaurant={restaurant} />
                         </Link>
                     ))}
                 </div>
-                {/* <button className="carousel-button left-button" onClick={scrollLeft}>&lt;</button>
-                <button className="carousel-button right-button" onClick={scrollRight}>&gt;</button> */}
+                <button className="carousel-button left-button" onClick={scrollLeft}>&lt;</button>
+                <button className="carousel-button right-button" onClick={scrollRight}>&gt;</button>
             </div>
         </div>
 
