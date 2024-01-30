ea0ad4c53374e59b8c4f4e61fe0262577dda29a9
Elliot Kuok <elliotckuok@gmail.com>
2024-01-30 13:24:34 -0800

    Make searchBar layout responsive to screen size

--- frontend/src/components/SearchBar/index.js
+++ frontend/src/components/SearchBar/index.js
@@ -97,16 +97,18 @@ const SearchBar = () => {
                         </select>
                     </div>
                 </div>
-                <div id="search-input-container">
-                    <svg className="search-icons" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M4.93 14.828a7 7 0 1 1 10.556-.757l3.939 3.94a.5.5 0 0 1 0 .707l-.707.707a.5.5 0 0 1-.707 0l-3.94-3.94a7.002 7.002 0 0 1-9.142-.657Zm1.413-8.485a5 5 0 1 0 7.071 7.071 5 5 0 0 0-7.07-7.07Z" fill="#2D333F"></path></svg>
-                    <input
-                        id="keyword-search"
-                        value={keyword}
-                        onChange={(e) => setKeyword(e.target.value)}
-                        placeholder={`Location, Restaurant, or Cuisine`}
-                    />
+                <div id="query-btn-container">
+                    <div id="search-input-container">
+                        <svg className="search-icons" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M4.93 14.828a7 7 0 1 1 10.556-.757l3.939 3.94a.5.5 0 0 1 0 .707l-.707.707a.5.5 0 0 1-.707 0l-3.94-3.94a7.002 7.002 0 0 1-9.142-.657Zm1.413-8.485a5 5 0 1 0 7.071 7.071 5 5 0 0 0-7.07-7.07Z" fill="#2D333F"></path></svg>
+                        <input
+                            id="keyword-search"
+                            value={keyword}
+                            onChange={(e) => setKeyword(e.target.value)}
+                            placeholder={`Location, Restaurant, or Cuisine`}
+                        />
+                    </div>
+                    <button type="submit">Let's go</button>
                 </div>
-                <button type="submit">Let's go</button>
             </div>
         </form>
     )
