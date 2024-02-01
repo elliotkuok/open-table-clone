e0b56ea8f5ad675914b92212165695b669ff83e7
Elliot Kuok <elliotckuok@gmail.com>
2024-02-01 14:50:15 -0800

    Add dynamic count for summer review count

--- frontend/src/components/RestaurantPage/index.js
+++ frontend/src/components/RestaurantPage/index.js
@@ -100,7 +100,7 @@ const RestaurantPage = () => {
                                         <path d="M19,4 L5,4 C3.8954305,4 3,4.8954305 3,6 L3,15 C3,16.1045695 3.8954305,17 5,17 L11,17 L15.36,20.63 C15.6583354,20.8784924 16.0735425,20.9318337 16.4250008,20.7668198 C16.776459,20.6018059 17.0006314,20.2482681 17,19.86 L17,17 L19,17 C20.1045695,17 21,16.1045695 21,15 L21,6 C21,4.8954305 20.1045695,4 19,4 Z M19,15 L15,15 L15,17.73 L11.72,15 L5,15 L5,6 L19,6 L19,15 Z" fill="#2D333F" fillRule="nonzero"></path>
                                         </svg>
                                     </div>
-                                    <div className='info-details'>Reviews</div>
+                                    <div className='info-details'>{filteredReviews.length} Review{filteredReviews.length === 1 ? "" : "s"}</div>
                                 </div>
                                 <div className='overview-info-component'>
                                     <div>
